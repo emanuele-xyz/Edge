45fd8d686c0888bd21c2711525bc84afa4b7f8bc
emanuele <me.emanuele.xyz@gmail.com>
2026-01-30 00:00:58 +0100

    chore: setup project

---
+++ Edge/src/Main.cpp
@@ -0,0 +1,8 @@
+#include <Edge\PCH.h>                                                                                                     
+
+int main()
+{ 
+	std::cout << "Hello, Edge!" << std::endl;                                                                        
+
+	return 0;                                                                                                         
+}
