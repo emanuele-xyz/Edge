909a647cbd15add7250de425aecda654cc04345e
emanuele <me.emanuele.xyz@gmail.com>
2026-03-12 23:52:38 +0100

    feat: implement basic math classes

--- Edge/include/Edge/Math.h
+++ Edge/include/Edge/Math.h
@@ -7,7 +7,7 @@ template <std::size_t N>
 class v
 {
 public:
-	constexpr v() noexcept : m_elems{} {}
+	constexpr v() noexcept : m_elems() {}
 	constexpr explicit v(const float* elems) noexcept : m_elems{} { std::copy_n(elems, N, m_elems); }
 	template <typename... Args>
 		requires (sizeof...(Args) == N) && (std::convertible_to<Args, float> && ...)
@@ -15,32 +15,75 @@ public:
 public:
 	constexpr const float& operator[](std::size_t i) const noexcept { return m_elems[i]; }
 	constexpr float& operator[](std::size_t i) noexcept { return m_elems[i]; }
-protected:
+public:
+	constexpr const float* Elems() const noexcept { return m_elems; }
+	constexpr float* Elems() noexcept { return m_elems; }
+private:
 	float m_elems[N];
 };
 
-
-class v2 : public v<2>
+template <typename Derived>
+class AccessorsXYRGST
 {
 public:
-	constexpr const float& x() const noexcept { return m_elems[0]; }
-	constexpr const float& y() const noexcept { return m_elems[1]; }
-	constexpr float& x() noexcept { return m_elems[0]; }
-	constexpr float& y() noexcept { return m_elems[1]; }
-	constexpr const float& u() const noexcept { return m_elems[0]; }
-	constexpr const float& v() const noexcept { return m_elems[1]; }
-	constexpr float& u() noexcept { return m_elems[0]; }
-	constexpr float& v() noexcept { return m_elems[1]; }
+	constexpr const float& x() const noexcept { return Self()[0]; }
+	constexpr const float& y() const noexcept { return Self()[1]; }
+	constexpr float& x() noexcept { return Self()[0]; }
+	constexpr float& y() noexcept { return Self()[1]; }
+public:
+	constexpr const float& r() const noexcept { return Self()[0]; }
+	constexpr const float& g() const noexcept { return Self()[1]; }
+	constexpr float& r() noexcept { return Self()[0]; }
+	constexpr float& g() noexcept { return Self()[1]; }
+public:
+	constexpr const float& s() const noexcept { return Self()[0]; }
+	constexpr const float& t() const noexcept { return Self()[1]; }
+	constexpr float& s() noexcept { return Self()[0]; }
+	constexpr float& t() noexcept { return Self()[1]; }
+protected:
+	constexpr Derived& Self() noexcept { return static_cast<Derived&>(*this); }
+	constexpr const Derived& Self() const noexcept { return static_cast<const Derived&>(*this); }
 };
 
-class v3 : public v<3>
+template <typename Derived>
+class AccessorsXYZRGBSTP : public AccessorsXYRGST<Derived>
 {
 public:
-	// TODO: to be continued
+	constexpr const float& z() const noexcept { return this->Self()[2]; }
+	constexpr float& z() noexcept { return this->Self()[2]; }
+public:
+	constexpr const float& b() const noexcept { return this->Self()[2]; }
+	constexpr float& b() noexcept { return this->Self()[2]; }
+public:
+	constexpr const float& p() const noexcept { return this->Self()[2]; }
+	constexpr float& p() noexcept { return this->Self()[2]; }
 };
 
-class v4 : public v<4>
+template <typename Derived>
+class AccessorsXYZWRGBASTP : public AccessorsXYZRGBSTP<Derived>
 {
 public:
-	// TODO: to be continued
+	constexpr const float& w() const noexcept { return this->Self()[3]; }
+	constexpr float& w() noexcept { return this->Self()[3]; }
+public:
+	constexpr const float& a() const noexcept { return this->Self()[3]; }
+	constexpr float& a() noexcept { return this->Self()[3]; }
+};
+
+class v2 : public v<2>, public AccessorsXYRGST<v2>
+{
+public:
+	using v<2>::v;
+};
+
+class v3 : public v<3>, public AccessorsXYZRGBSTP<v3>
+{
+public:
+	using v<3>::v;
+};
+
+class v4 : public v<4>, public AccessorsXYZWRGBASTP<v4>
+{
+public:
+	using v<4>::v;
 };
